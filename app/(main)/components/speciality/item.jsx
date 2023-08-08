52f966e00f082511f88c2cc24fadeb0f86042a8f
GeekyChunk <ermia1303@gmail.com>
2023-08-08 18:44:16 +0330

    few changes #2

--- app/(main)/components/speciality/item.jsx
+++ app/(main)/components/speciality/item.jsx
@@ -1,6 +1,6 @@
 const SpecialityItem = ({ title, icon }) => {
     return (
-        <div className="px-1 py-2 relative w-1/2 md:w-[14rem]">
+        <div className="px-1 py-2 relative w-1/2 md:w-[14rem] z-20">
             <div
                 className="hover:rotate-6 transform transition duration-300 border-2 border-transparent w-4/5 mx-auto bg-white hover:shadow-lg hover:scale-105 hover:shadow-gray-300/50 cursor-pointer rounded-2xl flex flex-col
                   items-center justify-center py-6">
@@ -9,8 +9,8 @@ const SpecialityItem = ({ title, icon }) => {
                 </div>
                 <h1 className="font-bold">{title}</h1>
             </div>
-            <div className="-z-50 absolute top-0 bottom-0 start-0 end-0">
-                <div className="w-full h-full bg-gradient-to-r from-pink-300 via-deep-purple-300 to-indigo-400 rounded-full blur-2xl opacity-60"></div>
+            <div className="-z-10 absolute top-0 bottom-0 start-0 end-0">
+                <div className="w-full h-full bg-gradient-to-r from-pink-300 via-deep-purple-300 to-indigo-400 rounded-full blur-2xl opacity-40"></div>
             </div>
         </div>
     )
