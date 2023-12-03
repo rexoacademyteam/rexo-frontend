acf7c4669564e8d1324ee00b5762ff32e528df82
Seyed Ahmad Mousavi <seyedahmadmosavi29@gmail.com>
2023-12-03 08:11:36 -0800

    footer updates

--- components/main/footer/index.jsx
+++ components/main/footer/index.jsx
@@ -98,32 +98,42 @@ export default function Footer() {
                 <div className="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600 overflow-hidden">
                     <div className="grid h-full max-w-lg grid-cols-5 mx-auto">
                         <button data-tooltip-target="tooltip-home" type="button" className="hover:bg-gray-200 inline-flex flex-col items-center justify-center px-5 rounded-l-full dark:hover:bg-gray-800 group">
+                            <a href="./courses">
                             <BookOpenIcon className="w-8 h-8" />
+                            </a>
                         </button>
                         <div id="tooltip-home" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
 
                         </div>
                         <button data-tooltip-target="tooltip-wallet" type="button" className="hover:bg-gray-200 inline-flex flex-col items-center justify-center px-5 dark:hover:bg-gray-800 group">
 
+                            <a href="./WhyRexo">
                             <QuestionMarkCircleIcon className="w-8 h-8" />
+                            </a>
 
                         </button>
                         <div id="tooltip-wallet" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
                         </div>
                         <div className="flex items-center justify-center">
                             <button data-tooltip-target="tooltip-new" type="button" onClick={clickHandler} className="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
+                                <a href="#top">
                                 <ChevronDoubleUpIcon className="w-7 h-7 text-white" />
+                                </a>
                             </button>
                         </div>
                         <div id="tooltip-new" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
                         </div>
                         <button data-tooltip-target="tooltip-settings" type="button" className="hover:bg-gray-200 inline-flex flex-col items-center justify-center px-5 dark:hover:bg-gray-800 group">
+                            <a href="contact">
                             <ArrowDownOnSquareIcon className="w-8 h-8" />
+                            </a>
                         </button>
                         <div id="tooltip-settings" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
                         </div>
                         <button data-tooltip-target="tooltip-profile" type="button" className="hover:bg-gray-200 inline-flex flex-col items-center justify-center px-5 rounded-r-full dark:hover:bg-gray-800 group">
+                            <a href="order">
                             <LinkIcon className="w-8 h-8" />
+                            </a>
                         </button>
                         <div id="tooltip-profile" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
                         </div>
